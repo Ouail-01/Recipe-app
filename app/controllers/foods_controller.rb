1227e2928eab0bc9a84a30920b1d5bb1c0a8e221
Ouail-01 <ouaillaarabi@gmail.com>
2023-08-01 20:47:44 +0100

    Add food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,27 @@
 class FoodsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.create(foods_params)
+    @food.user_id = current_user.id
+
+    if @food.save
+      redirect_to foods_path
+    else
+      render :new, status: 422
+    end
+  end
+
+  private
+
+  def foods_params
+    params.require(:food).permit(:id, :name, :measurement_unit, :price, :quantity)
+  end
 end

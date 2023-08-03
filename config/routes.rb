9775559032ca7c6d5f6484b2caaa356e623cb83c
Ouail-01 <ouaillaarabi@gmail.com>
2023-08-03 16:45:55 +0100

    Routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'foods#index'
+  root 'recipies#public_recipies'
   # Defines the root path route ("/")
   resources :foods, expect: [:update]
+  resources :recipies, expect: [:update]
   # root "articles#index"
+  get '/public_recipies', to: 'recipies#public_recipies'
 end

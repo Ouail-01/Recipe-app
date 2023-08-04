51bb81579a1c6fc7117263520e299582c4f235f0
Ouail-01 <ouaillaarabi@gmail.com>
2023-08-04 16:07:08 +0100

    Update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'recipes#index'
+  root 'recipes#public_recipes'
   # Defines the root path route ("/")
   resources :foods, expect: [:update]
   resources :recipes, expect: [:update]
 
   get '/public_recipes', to: 'recipes#public_recipes'
+  get '/shopping_list', to: 'shopping_list#index'
 
   get '/toggle/:id', to: 'recipes#toggle'
 
